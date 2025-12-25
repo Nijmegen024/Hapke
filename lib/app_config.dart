3284999d433e9ff2c419fe288db1d9ffb1e47e8c
Aeren <0aeren2kooijman4@gmail.com>
2025-12-25 11:21:12 +0100

    chore(config): set google web client id

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,5 +1,6 @@
 class AppConfig {
   static const apiBaseUrl = 'https://hapke-backend.onrender.com';
   // TODO: vul je Google OAuth client ID (Web) in en zet dezelfde in backend env GOOGLE_CLIENT_ID.
-  static const googleWebClientId = '';
+  static const googleWebClientId =
+      '243472214784-akcpugniq1eaf2ib5ao1ihflq6qfqk4o.apps.googleusercontent.com';
 }
