ec9502942ff79a4f7bc028fc20e188f1d239141f
Aeren <0aeren2kooijman4@gmail.com>
2026-01-04 19:29:36 +0100

    deploy: update flutter web build with new color #2aaab3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3380892622"
+    serviceWorkerVersion: "2301736562"
   }
 });
